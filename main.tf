608b4c420b3b14b857c5478ad16836e2bb0760a5
Heidi Health SRE <sre@heidihealth.com>
2026-01-17 16:54:08 +1000

    chore(terraform): add initial provider/vars configuration

---
+++ main.tf
@@ -0,0 +1,9 @@
+/*
+  Heidi Health - Terraform Practical Exercise
+
+  This project provides a small baseline Terraform structure.
+*/
+
+terraform {
+  required_version = ">= 1.6.0"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region   = "us-east-1"
+project_name = "heidihealth_technical_interview"
+environment  = "dev"
